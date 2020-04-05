47e975d5be9e292c9c66fa26f705d8c16ace48dd
Santiago Garcia <santiagogarcia97@outlook.com>
2020-04-05 15:14:39 -0300

    1.3 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,26 +3,29 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course} />
             <Content part1={part1}
-                     ex1={exercises1}
                      part2={part2}
-                     ex2={exercises2}
                      part3={part3}
-                     ex3={exercises3}
             />
-            <Total  ex1={exercises1}
-                    ex2={exercises2}
-                    ex3={exercises3}
+            <Total  ex1={part1.exercises}
+                    ex2={part2.exercises}
+                    ex3={part3.exercises}
             />
         </div>
     )
@@ -37,16 +40,16 @@ const Header = (args) => {
 const Content = (args) => {
     return (
         <>
-            <Part part={args.part1} ex={args.ex1}/>
-            <Part part={args.part2} ex={args.ex2}/>
-            <Part part={args.part3} ex={args.ex3}/>
+            <Part part={args.part1} />
+            <Part part={args.part2} />
+            <Part part={args.part3} />
         </>
     )
 }
 
 const Part = (args) => {
     return (
-        <p>{args.part} {args.ex}</p>
+        <p>{args.part.name} {args.part.exercises}</p>
     )
 }
 
