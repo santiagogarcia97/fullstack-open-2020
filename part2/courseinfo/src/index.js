78d524d694b6f040eeb179bd7e5f3a5407e1b9ee
Santiago Garcia <santiagogarcia97@outlook.com>
2020-04-06 19:37:23 -0300

    2.4 done

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -2,53 +2,72 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Course course={course} />
+            <h1>Web Development Courses</h1>
+            {courses.map(course => <Course key={course.id} course={course} />)}
         </div>
     )
 }
 
 const Course = ({course}) => {
     return(
-        <>
+        <div>
             <Header name={course.name}/>
             <Content parts={course.parts}/>
             <Total parts={course.parts}/>
-        </>
+        </div>
     )
 }
 
 const Header = ({name}) => {
     return (
-        <h1>{name}</h1>
+        <h2>{name}</h2>
     )
 }
 
@@ -70,11 +89,11 @@ const Part = ({part}) => {
 
 const Total = ({parts}) => {
     let total = parts.reduce((sum, part) => sum + part.exercises, 0)
-    
+
     return (
-        <h2>
+        <h3>
             <p>Number of exercises {total}</p>
-        </h2>
+        </h3>
     )
 }
 
