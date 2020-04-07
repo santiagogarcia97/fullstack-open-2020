b5b4f2d57fd479f17f72c607b4e595d5c7ff2c6a
Santiago Garcia <santiagogarcia97@outlook.com>
2020-04-07 00:14:54 -0300

    2.6 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+      setNewName(event.target.value)
+  }
+  
+  const addName = (event) => {
+      event.preventDefault()
+      const newPerson = {
+          name: newName
+      }
+      setPersons(persons.concat(newPerson))
+  }
+
+  return (
+      <div>
+        <h2>Phonebook</h2>
+
+          <form>
+          <div>
+            <p>name: <input value={newName} onChange={handleNameChange} /></p>
+          </div>
+          <div>
+            <button type="submit" onClick={addName}>add</button>
+          </div>
+        </form>
+
+        <h2>Numbers</h2>
+          {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      </div>
+  )
+}
+
+export default App
