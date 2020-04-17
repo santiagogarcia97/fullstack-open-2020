539387d7911ee19cba2940fe2f2d429bb917a72e
Santiago Garcia <santiagogarcia97@outlook.com>
2020-04-17 15:43:52 -0300

    4.17 done

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,16 +1,31 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({})
-  res.json(blogs)
+    .populate('user', { username: 1, name: 1, id: 1 })
+  res.json(blogs.map(b => b.toJSON()))
 })
 
-blogRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+blogRouter.post('/', async (req, res, next) => {
+  const body = req.body
+  const user = await User.findOne({})
+//  if(!user) next({message: 'invalid user', statusCode: 400})
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
 
   const result = await blog.save()
-  res.status(201).json(result)
+  user.blogs = user.blogs.concat(result._id)
+
+  await user.save()
+  res.status(201).json(result.toJSON())
 })
 
 blogRouter.delete('/:id', async (req, res) => {
--- part4/bloglist/backend/controllers/users.js
+++ part4/bloglist/backend/controllers/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user')
 
 usersRouter.get('/', async (req, res) => {
   const users = await User.find({})
+    .populate('blogs', { title: 1, author: 1, url: 1, id: 1 })
   res.json(users.map(u => u.toJSON()))
 })
 
