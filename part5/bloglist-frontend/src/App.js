f3058fb547f4f512c52cc96ad15f117855b98729
Santiago Garcia <santiagogarcia97@outlook.com>
2020-04-21 18:08:28 -0300

    5.2 done

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -16,6 +16,9 @@ const App = () => {
         username, password,
       })
 
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user))
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -28,10 +31,29 @@ const App = () => {
     }
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogUser')
+
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
+  }, [])
+
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      //noteService.setToken(user.token)
+    }
   }, [])
 
   if (user === null) {
@@ -59,7 +81,10 @@ const App = () => {
   return(
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
+        <p>
+          {user.name} logged in
+          <button onClick={handleLogout}>Logout</button>
+        </p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
