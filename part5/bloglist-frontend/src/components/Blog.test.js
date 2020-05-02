9b9267fa2db982080d26bb8ef7297e467826c2c8
Santiago Garcia <santiagogarcia97@outlook.com>
2020-05-02 01:02:43 -0300

    5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 const blog = {
   title: 'Test title',
   author: 'Test author',
   url: 'Test url',
+  likes: 10,
   user: {username: 'Test username'}
+}
+
+const user = {
+  username: 'Test username'
 }
 
 describe('testing Blog component', () => {
@@ -16,8 +21,20 @@ describe('testing Blog component', () => {
     const component = render(
       <Blog blog={blog}></Blog>
     )
-    component.debug()
     expect(component.container).toHaveTextContent('Test title')
     expect(component.container).toHaveTextContent('Test author')
+  })
+  test('renders extra content when button clicked', () => {
+
+    const component = render(
+      <Blog blog={blog} user={user}></Blog>
+    )
+    component.debug()
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('Test url')
+    expect(component.container).toHaveTextContent('10')
   })
 })
