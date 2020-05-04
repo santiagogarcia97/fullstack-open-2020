f485d649ab0a0d84df8859ef4a0f5035041cf291
Santiago Garcia <santiagogarcia97@outlook.com>
2020-05-04 16:12:37 -0300

    6.19 done

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,29 +1,36 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import {connect, useDispatch} from 'react-redux'
 import { voteAnecdote } from  '../reducers/anecdoteReducer'
 import  {setNotification} from '../reducers/notificationReducer'
 
-const AnecdoteList = () => {
-  const anecdotes = useSelector(({anecdotes, filter}) => {
-    if(filter !== '') {
-      anecdotes = anecdotes.filter((a) => a.content.indexOf(filter) !== -1)
+const AnecdoteList = (props) => {
+  // const anecdotes = useSelector(({anecdotes, filter}) => {
+  //   if(filter !== '') {
+  //     anecdotes = anecdotes.filter((a) => a.content.indexOf(filter) !== -1)
+  //   }
+  //   return anecdotes.sort((a, b) => b.votes - a.votes)
+  // })
+  const dispatch = useDispatch()
+
+
+  const anecdotesToShow = () => {
+    let anecdotes
+    if(props.filter !== '') {
+      anecdotes = props.anecdotes.filter((a) => a.content.indexOf(props.filter) !== -1)
+    } else {
+      anecdotes = props.anecdotes
     }
     return anecdotes.sort((a, b) => b.votes - a.votes)
-  })
-  const dispatch = useDispatch()
+  }
 
   const vote = anecdote => {
     dispatch(voteAnecdote(anecdote))
     dispatch(setNotification(`You voted "${anecdote.content}"`, 5))
-    // dispatch(setMessage(`You voted "${anecdote.content}"`))
-    // setTimeout(() => {
-    //   dispatch(clearMessage())
-    // }, 5000)
   }
 
   return(
     <div>
-      {anecdotes.map(anecdote =>
+      {anecdotesToShow().map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -38,4 +45,14 @@ const AnecdoteList = () => {
   )
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  // sometimes it is useful to console log from mapStateToProps
+  console.log(state)
+  return {
+    anecdotes: state.anecdotes,
+    filter: state.filter
+  }
+}
+
+const ConnectedAnecdotes = connect(mapStateToProps)(AnecdoteList)
+export default ConnectedAnecdotes
