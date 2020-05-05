803079a0502de8b88e3466055f3be723d44befcb
Santiago Garcia <santiagogarcia97@outlook.com>
2020-05-05 20:13:38 -0300

    7.4 done

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,42 +1,46 @@
-import React, {useState} from 'react'
+import React from 'react'
+import {useField} from '../hooks'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
 
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
 
   return (
     <div>
-      <h2>create a new anecdote</h2>
+      <h2>Create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
+        <table>
+          <tbody>
+          <tr>
+            <td>Content</td>
+            <td><input {...content} /></td>
+          </tr>
+          <tr>
+            <td>Author</td>
+            <td><input {...author} /></td>
+          </tr>
+          <tr>
+            <td>URL for more info</td>
+            <td><input {...info} /></td>
+          </tr>
+          </tbody>
+        </table>
+        <button type='submit'>Create</button>
       </form>
     </div>
   )
-
 }
 
 export default CreateNew
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,5 @@
+import useField from './useField'
+
+export {
+  useField
+}
