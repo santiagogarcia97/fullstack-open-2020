c18bd4565d166523ce0bc2877dd5ddad2e01ae67
Santiago Garcia <santiagogarcia97@outlook.com>
2020-05-10 17:38:44 -0300

    8.4 done

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -94,7 +94,7 @@ const typeDefs = gql`
     type Query {
         authorCount: Int!
         bookCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String): [Book]
         allAuthors: [Author]!
     }
 `
@@ -103,7 +103,13 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(args.author) {
+        return books.filter(book => book.author === args.author)
+      } else {
+        return books;
+      }
+    },
     allAuthors: () => {
       return authors.map(author => {
         const bookCount = books.reduce((n, book) => {return n + (book.author === author.name)}, 0)
