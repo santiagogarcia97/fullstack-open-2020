d88ea6b337664979d07bedc1f69447a73266f10e
Santiago Garcia <santiagogarcia97@outlook.com>
2020-05-14 12:22:04 -0300

    9.10 done

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from "cors";
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 
 app.use(cors())
 app.use(express.json());
-
-const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
+app.use('/api/diagnoses', diagnosesRouter);
+
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
